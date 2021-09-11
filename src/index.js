e26e6f5f2f0643e446930230c179bee4ab5f67af
bryon czoch <czochb@gmail.com>
2021-09-10 23:48:28 -0400

    remove fApp hack

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
-class fApp extends React.Component {
-render(){
-    return (
-      <Router history={browsewrHistory}>
-          <Route path={"/"} component={Root} >
-              <IndexRoute component={Home} />
-              <Route path={"user"} component={User} />
-              <Route path={"home"} component={Home} />
-          </Route>
-          <Route path={"home-single"} component={Home}/>
-      </Router>
-    );
-  }
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-render(<fApp />, window.document.getElementById('app'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
